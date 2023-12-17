3767dd4e8ab986a298e4197b649cf7abc2b835b4
Amit8127 <amitmonu0000@gmail.com>
2023-12-18 01:04:27 +0530

    5Apis

---
+++ src/test/java/com/apiIntegration/Api/Integration/ThirdPartyApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiIntegration.Api.Integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPartyApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
